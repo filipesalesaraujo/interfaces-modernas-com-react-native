6d1a57dbeb2dce650ad389b0a5355227584b8486
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-28 08:52:47 -0300

    theme provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,30 @@
 import React from "react";
-import { Home } from "./src/screens/Home";
+import { Home } from "./src/components/screens/Home";
+import { ThemeProvider } from "styled-components/native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import THEME from "./src/components/themes";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={THEME}>
+      <Home />;
+    </ThemeProvider>
+  );
 }
